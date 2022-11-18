a027a7c73428c6a74443a5de7974b4070167d0ea
Srikant Shubham <srikantsubham10000@gmail.com>
2022-11-18 16:03:40 +0530

    new interface

--- weather/weather/settings.py
+++ weather/weather/settings.py
@@ -117,7 +117,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+BASE_DIR = Path(__file__).resolve().parent.parent
 
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = (
+	# '/home/u1r60cp6vyvs/greenvasa/static',
+    STATIC_DIR,
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- weather/weather_app/views.py
+++ weather/weather_app/views.py
@@ -7,7 +7,7 @@ import requests
 def index(request):
     # https://openweathermap.org/  --Create an account here to get the api key
     # url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=API_KEY' -- we have to give the API_KEY
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=1db02d732975aa1e12465725dae8b5da'
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=1db02d732975aa1e12465725dae8b5da'
     if request.method=="POST":
         city = request.POST.get('city')
         print(city)
@@ -18,8 +18,11 @@ def index(request):
             'city' : city,
             'temperature' : city_weather['main']['temp'],
             'description' : city_weather['weather'][0]['description'],
-            'icon' : city_weather['weather'][0]['icon']
+            'icon' : city_weather['weather'][0]['icon'],
+            'humidity':city_weather['main']['humidity'],
+            'wind':city_weather['wind']['speed']
         }
+        print(weather)
         return render(request, 'index.html', {'weather' : weather})
     return render(request, 'index.html') #returns the index.html template
 
